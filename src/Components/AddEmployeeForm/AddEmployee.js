9ef30d18b2df41500883d6ac6b66d67a103dd56d
Codex-Navi <teamsoftwareemail@gmail.com>
2025-01-24 16:26:56 +0530

    update

--- src/Components/AddEmployeeForm/AddEmployee.js
+++ src/Components/AddEmployeeForm/AddEmployee.js
@@ -51,7 +51,7 @@ const dispatch=useDispatch()
         const response = await fetch(`https://navicompu.co.in/api/createuser/`,{
             method:"POST",
             body:JSON.stringify(req),
-            header:{
+            headers:{
               'Content-type':'application/json',
         
             }
@@ -59,6 +59,7 @@ const dispatch=useDispatch()
         });
         const data = await response.json();
         console.log('employe response',data)
+        console.log('employe response',req)
     };
 
     const handleTogglePasswordVisibility = () => {
